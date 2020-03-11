dfb15715a5c36f196dd04d9447232fd808673af8
Samantha Barros <samantha.barros95@gmail.com>
2020-03-11 19:52:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,8 @@
 class Dog
   
-  def breed=(breed)
-    breed = @breed
-  end
-  
-  def breed 
-    @breed
-  end
-  
   def initialize(name, breed = "Mutt")
     @name = name
-  end
-  
-  def initialize(breed)
-    @breed = "Mutt"
+    @breed = breed
   end
   
 end
